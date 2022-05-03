a85a710d1789f0298b10d16cc857ae1781549ddd
abhijeet <abhijeetsinha43@gmail.com>
2022-05-03 20:45:06 +0530

    feat: added trash screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Notes, Archive } from "./Pages/";
+import { Home, Notes, Archive, Trash } from "./Pages/";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/notes" element={<Notes />} />
           <Route path="/archive" element={<Archive />} />
+          <Route path="/trash" element={<Trash />} />
         </Routes>
       </Router>
     </>
---
+++ src/Pages/Trash/Trash.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Trash.css";
+import { SideNav, NotesCard, Header } from "../../components";
+
+const Trash = () => {
+  return (
+    <>
+      <main className="main-section">
+        <SideNav />
+        <section className="notes-content">
+          <Header />
+          <section className="section-title-container">
+            <h2 className="section-title">Trash</h2>
+          </section>
+          <section className="cards-container">
+            <NotesCard />
+            <NotesCard />
+            <NotesCard />
+          </section>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export { Trash };
